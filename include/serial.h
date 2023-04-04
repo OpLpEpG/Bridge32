dc7b849227d7c7958d0f6125a433a45501e84a94
Олег Андрианов <ooorivas@hotmail.com>
2023-04-04 10:41:54 +0500

    2 commit

--- include/serial.h
+++ include/serial.h
@@ -1,12 +1,7 @@
 
 #ifndef SERIAL2_H
 #define SERIAL2_H
-
 #pragma once
-
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
 
 #define UART_BUF_LEN 0x10000
 extern uint8_t buf[UART_BUF_LEN];
@@ -17,9 +12,5 @@ extern volatile bool UartBreack;
 void Uart2Start(void);
 void Uart2TurboHandler(void);
 void Uart2Send(const uint8_t* buf, size_t len);
-
-// #ifdef __cplusplus
-// }
-// #endif
 
 #endif
